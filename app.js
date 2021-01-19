662efa6890bb15dcc2fc05bf481c4fd8f07ee868
Javier Requejo <javier@alternaenergetica.com>
2021-01-19 17:39:23 +0000

    Initial commit with project structure done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./lib/routes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.listen('3020', () => {
+    console.log(`Server running on port '3020'...`)
+})
+
+module.exports = app;
