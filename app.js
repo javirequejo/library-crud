f46bd61b0da14f457af336021247f1bba610b3de
Javier Requejo <javier@alternaenergetica.com>
2021-01-20 00:41:51 +0000

    Listing all rows from db to router

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 app.listen('3000', () => {
-    console.log(`Server running on port '3020'...`)
+    console.log(`Server running on port '3000'...`)
 })
 
 module.exports = app;
